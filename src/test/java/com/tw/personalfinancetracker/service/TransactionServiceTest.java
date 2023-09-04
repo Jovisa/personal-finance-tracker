12be233bc75a34c1abc0c4a36edeb52bdc8b7da9
Jovan-Stepanovic <Jovan.Stepanowic@nordstrom.com>
2023-09-04 04:30:56 +0200

    - Service coverred with tests - refactored repository

---
+++ src/test/java/com/tw/personalfinancetracker/service/TransactionServiceTest.java
@@ -0,0 +1,60 @@
+package com.tw.personalfinancetracker.service;
+
+import com.tw.personalfinancetracker.model.Transaction;
+import com.tw.personalfinancetracker.repository.TransactionsRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+
+@SpringBootTest
+class TransactionServiceTest {
+
+    @MockBean
+    private TransactionsRepository repository;
+
+    @Autowired
+    private TransactionService service;
+
+    @Test
+    public void serviceReturnsDataFromRepositoryTest() {
+
+        List<Transaction> transactions = List.of(
+                new Transaction( 1L, "income", 1.0, ""),
+                new Transaction(2L, "expense", 2.0, "")
+        );
+
+        Mockito.when(repository.findAll()).thenReturn(transactions);
+        assertEquals(transactions, service.getAllTransactions());
+
+        service.deleteTransaction(2L);
+        assertEquals(transactions, service.getAllTransactions());
+    }
+
+    @Test
+    public void serviceCallsDeleteMethodOfRepositoryTest() {
+        service.deleteTransaction(1L);
+        Mockito.verify(repository, times(1)).deleteById(1L);
+    }
+
+    @Test
+    public void serviceCallsRepositoryToUpdateTransaction() {
+        Transaction transaction = new Transaction( 1L, "income", 1.0, "");
+        service.update(transaction);
+        Mockito.verify(repository, times(1)).save(transaction);
+    }
+
+    @Test
+    public void serviceCallRpositoryToAddNewTransactionTest() {
+        Transaction transaction = new Transaction( 1L, "income", 1.0, "");
+        service.save(transaction);
+        Mockito.verify(repository, times(1)).save(transaction);
+    }
+
+}
